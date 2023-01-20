a3bc9faf2338eae34add57e72e0598e65b47862e
Rupam Gupta <guptarupam.1020@gmail.com>
2023-01-21 01:26:20 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Store Finder</h1>
+        <h3>Here you Find the Nearest Store</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
